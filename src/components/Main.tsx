a34ce7f369d71f54386294208e35f86f39d40831
Marharyta <rita.shyshkavets@gmail.com>
2025-07-09 13:05:07 +0200

    chore: setup project with Vite, ESLint, Prettier and Husky

---
+++ src/components/Main.tsx
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return <div className="text-5xl text-amber-700">Hello, World</div>;
+  }
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import Main from './components/Main.tsx';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Main />
+  </StrictMode>
+);
