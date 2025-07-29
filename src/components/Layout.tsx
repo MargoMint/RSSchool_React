1be045c77e44cecd3c9e116e968e380b8a63049c
Marharyta <rita.shyshkavets@gmail.com>
2025-07-29 17:20:46 +0200

    refactor: replace ReactNode children prop with PropsWithChildren

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,6 @@
-import React from 'react';
+import type { PropsWithChildren } from 'react';
 
-interface LayoutProps {
-  children: React.ReactNode;
-}
-
-function Layout({ children }: LayoutProps) {
+function Layout({ children }: PropsWithChildren) {
   return <div className="max-w-4xl mx-auto pt-4 px-4">{children}</div>;
 }
 
