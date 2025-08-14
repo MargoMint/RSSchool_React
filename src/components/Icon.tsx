e8d21009a17ef41b7ec8ec27476f4eb4bd4544bd
Marharyta <rita.shyshkavets@gmail.com>
2025-08-14 12:50:21 +0200

    refactor: extract Icon component for theme switching

---
+++ src/components/Icon.tsx
@@ -0,0 +1,19 @@
+import useTheme from '../hooks/useTheme';
+
+interface IconProps {
+  lightThemeIcon: string;
+  darkThemeIcon: string;
+}
+
+function Icon({ lightThemeIcon, darkThemeIcon }: IconProps) {
+  const { theme } = useTheme();
+
+  const themeIcon = theme === 'light' ? lightThemeIcon : darkThemeIcon;
+
+  const themeDescription =
+    theme === 'light' ? 'Switch to dark theme' : 'Switch to light theme';
+
+  return <img src={themeIcon} alt={themeDescription} className="w-8 h-8" />;
+}
+
+export default Icon;
