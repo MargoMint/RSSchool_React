8c1065c2ee2d5f14620b6fa2d2b8e7bd9c6e31e0
Marharyta <rita.shyshkavets@gmail.com>
2025-08-16 18:32:34 +0200

    fix: wrap page with Layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,15 @@
-/* import Layout from '../components/Layout'; */
+import Layout from '../components/Layout';
 
 function NotFoundPage() {
   return (
-    <div className="min-h-screen flex gap-5 items-center justify-center">
-      <h2 className="font-extrabold text-red-800 text-3xl pr-5 border-r-4">
-        404 Not Found
-      </h2>
-      <p>Oops, there is no such page</p>
-    </div>
+    <Layout>
+      <div className="min-h-screen flex gap-5 items-center justify-center">
+        <h2 className="font-extrabold text-red-800 text-3xl pr-5 border-r-4">
+          404 Not Found
+        </h2>
+        <p>Oops, there is no such page</p>
+      </div>
+    </Layout>
   );
 }
 
