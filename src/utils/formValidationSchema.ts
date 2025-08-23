7a077c857f87095857ce8f07c22ae0a87ff759e5
Marharyta <rita.shyshkavets@gmail.com>
2025-08-23 19:30:51 +0200

    feat: add form validation schema using Yup

---
+++ src/utils/formValidationSchema.ts
@@ -0,0 +1,56 @@
+import * as yup from 'yup';
+
+const formValidationSchema = yup.object({
+  name: yup
+    .string()
+    .matches(/^[A-Z][a-zA-Z]*$/, 'Name must start with an uppercase letter')
+    .required('Name is required'),
+  age: yup
+    .number()
+    .positive('Age should not have a negative value')
+    .integer('Age must be an integer')
+    .required('Age is required'),
+  email: yup.string().email('Invalid email').required('Email is required'),
+  password: yup
+    .string()
+    .min(4, 'Password must contain at least 4 characters')
+    .matches(
+      /(?=.*[a-z])/,
+      'Password must contain at least one lowercase letter'
+    )
+    .matches(
+      /(?=.*[A-Z])/,
+      'Password must contain at least one uppercase letter'
+    )
+    .matches(/(?=.*[0-9])/, 'Password must contain at least one number')
+    .matches(
+      /(?=.*[!@#$%^&*])/,
+      'Password must contain at least one special character (!@#$%^&*)'
+    )
+    .required('Password is required'),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref('password')], 'Passwords must match')
+    .required('Matching passwords is required'),
+  gender: yup.string().required('Gender is required'),
+  acceptTermsAndCondition: yup
+    .boolean()
+    .oneOf([true], 'Terms and Conditions must be accepted')
+    .required('Terms and Conditions are required'),
+  picture: yup
+    .mixed<File>()
+    .required('Picture is required')
+    .test('fileSize', 'The file is too large (max 20 MB)', (value) => {
+      return value && value.size <= 20 * 1024 * 1024;
+    })
+    .test(
+      'fileFormat',
+      'Unsupported file format. Only .jpeg and .png are allowed',
+      (value) => {
+        return value && ['image/jpeg', 'image/png'].includes(value.type);
+      }
+    ),
+  country: yup.string().required('Country is required'),
+});
+
+export default formValidationSchema;
