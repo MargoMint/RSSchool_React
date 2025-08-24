807499d7128df0ca873c74f6fc4e51bbd84ad18e
Marharyta <rita.shyshkavets@gmail.com>
2025-08-24 20:08:23 +0200

    refactor: remove unused selectors

--- src/store/formSelectors.ts
+++ src/store/formSelectors.ts
@@ -3,11 +3,3 @@ import type { StoredFormData } from '../components/Forms/formTypes';
 
 export const selectSubmissions = (state: RootState): StoredFormData[] =>
   state.forms.submissions;
-
-export const selectLastSubmission = (
-  state: RootState
-): StoredFormData | undefined =>
-  state.forms.submissions[state.forms.submissions.length - 1];
-
-export const selectSubmissionsCount = (state: RootState): number =>
-  state.forms.submissions.length;
