d7b1a8588a21a261cef4fdff0e2789a6425f3dcb
Marharyta <rita.shyshkavets@gmail.com>
2025-08-25 00:35:54 +0200

    test: add unit tests for Layout component

---
+++ src/tests/Layout.test.tsx
@@ -0,0 +1,21 @@
+import { describe, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Layout from '../components/Layout/Layout';
+
+describe('Layout', () => {
+  test('renders without crashing', () => {
+    render(<Layout>Test content</Layout>);
+    const contentElement = screen.getByText('Test content');
+    expect(contentElement).toBeInTheDocument();
+  });
+
+  test('renders children correctly', () => {
+    render(
+      <Layout>
+        <p>Children</p>
+      </Layout>
+    );
+    const childElement = screen.getByText('Children');
+    expect(childElement).toBeInTheDocument();
+  });
+});
